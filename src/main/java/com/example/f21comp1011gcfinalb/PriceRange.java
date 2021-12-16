d58992a06ff2b83d6153f979b7cd4c7eb96be377
DakotaMH21 <dakota_mh@hotmail.com>
2021-12-16 10:25:16 -0500

    Question 4c complete

--- src/main/java/com/example/f21comp1011gcfinalb/PriceRange.java
+++ src/main/java/com/example/f21comp1011gcfinalb/PriceRange.java
@@ -1,4 +1,42 @@
 package com.example.f21comp1011gcfinalb;
 
 public class PriceRange {
+    private int min;
+    private int max;
+
+    public PriceRange(int min, int max){
+        this.min = min;
+        this.max = max;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        if(min > 0){
+            this.min = min;
+        }
+        else{
+            throw new IllegalArgumentException("Minimum must be greater than 0");
+        }
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        if(max > min){
+            this.max = max;
+        }
+        else{
+            throw new IllegalArgumentException("Maximum must be greater than minimum price");
+        }
+    }
+
+
+    public String toString(){
+        return String.format("$%d to $%d", min, max);
+    }
 }
