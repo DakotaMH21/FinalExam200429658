20cbb0cf26b4faf7b2a86536acac72bd8e714871
DakotaMH21 <dakota_mh@hotmail.com>
2021-12-16 08:18:46 -0500

    Initial commit

---
+++ src/main/java/com/example/f21comp1011gcfinalb/Car.java
@@ -0,0 +1,5 @@
+package com.example.f21comp1011gcfinalb;
+
+public class Car {
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.f21comp1011gcfinalb {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+
+    opens com.example.f21comp1011gcfinalb to javafx.fxml;
+    exports com.example.f21comp1011gcfinalb;
+}
